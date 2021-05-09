661351c254a4f50b2df10ef0c41cb6052f79725a
daring-demo-aacc <azizrocke@gmail.com>
2021-05-09 14:06:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,19 +11,22 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
+  let animal = "cat"
   return animal
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
 var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
+  return function theFunk(name) {
+  var  name = 'FUNKY!' 
+  return name
   }
 }
 
